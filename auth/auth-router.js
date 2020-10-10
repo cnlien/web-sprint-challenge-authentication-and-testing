8501b319d883d8b38bfcdfbe6dffa2b75209bd76
Chris Lien <clien@sharpencx.com>
2020-10-10 12:14:52 -0400

    Added register function to auth-route.js

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,7 +1,25 @@
 const router = require('express').Router();
+const bcrypt = require('bcryptjs');
+const Users = require('../user/user-model.js');
 
 router.post('/register', (req, res) => {
-  // implement registration
+  let user = req.body
+  const hash = bcrypt.hashSync(user.password, 10);
+  user.password = hash;
+
+  Users.add(user)
+    .then((saved) => {
+      res.status(201).json({
+        message: "The user has been saved to the system",
+        saved
+      })
+    })
+    .catch((err) => {
+      res.status(500).json({
+        message: 'There was an error saving the user',
+        error: err
+      })
+    })
 });
 
 router.post('/login', (req, res) => {
